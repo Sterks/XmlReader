2705eab38dd4e07939446c5c3116da53538ef565
Denis S. Runov <it-saratov@mail.ru>
2019-10-11 17:03:24 +0300

    init

--- internal/app/db/init.go
+++ internal/app/db/init.go
@@ -10,17 +10,18 @@ type ConnectionString struct {
 	ConnectionString string
 }
 
-type pgDb struct {
+//PgDb ...
+type PgDb struct {
 	dbConn *sqlx.DB
 }
 
 // InitDb ...
-func InitDb(cfg ConnectionString) (*pgDb, error) {
+func InitDb(cfg ConnectionString) (*PgDb, error) {
 	dbConn, err := sqlx.Connect("postgres", cfg.ConnectionString)
 	if err != nil {
 		return nil, err
 	}
 
-	p := &pgDb{dbConn: dbConn}
+	p := &PgDb{dbConn: dbConn}
 	return p, nil
 }
